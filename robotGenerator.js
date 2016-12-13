6c766e21f99d44c6b503ed69dcba77cb5d4bcdc1
VincentEPN <vincent.laroche@paysdemartigues.fr>
2016-12-13 15:50:49 +0100

    Update robotGenerator.js

--- robotGenerator.js
+++ robotGenerator.js
@@ -25,9 +25,9 @@ Blockly.Arduino ['Tourneenrond'] = function (block)  {
   Blockly.Arduino.setups_["setup_robotdulab"] = setup_robotdulab;
   Blockly.Arduino.definitions_["define_robotdulab"] = define_robotdulab;
   var code  ="robotDuLAB.Avancer();\n";  
-  code  +="delay("50");\n";
+  code  +="delay(50);\n";
   code  +="robotDuLAB.TournerADroite();\n";
-  code  +="delay("50");\n";
+  code  +="delay(50);\n";
   return  code;
 };
 
